f7f4be1fe6f966ef2a81e6383b5ddd3510b7181e
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-08-04 11:43:22 +0300

    Add multiarch support.

--- tools/build.sh
+++ tools/build.sh
@@ -6,5 +6,8 @@ cd "$1/src"
 echo "I: Installing build-dependencies..."
 mk-build-deps -i -r --tool="apt-get --no-install-recommends -y"
 
+echo -n "I: Check if we're building multiarch..."
+egrep -q "^Architecture: (any|i386)" debian/control && echo "yes" && dpkg-buildpackage -us -uc -ai386 || echo "no"
+
 echo "I: Building the package..."
 dpkg-buildpackage -us -uc
