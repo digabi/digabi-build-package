e678aeeefa2c30308dcbaa3c82020ff1cbfd2245
Ville Korhonen <ville@xd.fi>
2015-09-10 01:23:18 +0300

    Add path to control file

--- tools/build.sh
+++ tools/build.sh
@@ -4,7 +4,7 @@ set -ex
 cd "$1/src"
 
 echo "I: Installing build-dependencies..."
-mk-build-deps -i -r --tool="apt-get --no-install-recommends -y"
+mk-build-deps -i -r --tool="apt-get --no-install-recommends -y" debian/control
 
 echo -n "I: Check if we're building multiarch..."
 egrep -q "^Architecture: (any|i386)" debian/control && echo "yes" && dpkg-buildpackage -us -uc -ai386 || echo "no"
